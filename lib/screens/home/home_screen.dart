badc2ad2fcd933bcffdafc626e3ab501ee3cda50
amr <amramrnour2255@gmail.com>
2023-10-31 17:44:49 +0200

    finished tab .

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/data/api/api_manager.dart';
+import 'package:news_app/screens/tabs/news/news_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routName ="home";
@@ -12,9 +13,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Welcome Mr Amr"),
+      appBar: AppBar(title: Text("News app"),
         centerTitle: true,
       ),
+      body: NewsTab(),
     );
   }
 }
